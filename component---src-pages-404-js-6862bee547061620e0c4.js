f9b540fe37b232a12fe91fef0369f5e481c1309b
Tony T <jsbot@pm.me>
2020-09-13 13:08:49 +1000

    Updates

---
+++ component---src-pages-404-js-6862bee547061620e0c4.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[4],{PGGC:function(e,t,n){},w2l6:function(e,t,n){"use strict";n.r(t);var a=n("q1tI"),u=n.n(a),l=n("L12J"),o=n("dmsj");n("PGGC");t.default=function(){return u.a.createElement(l.a,null,u.a.createElement(o.a,{title:"404: Not found"}),u.a.createElement("h1",null,"NOT FOUND"),u.a.createElement("p",null,"You just hit a route that doesn't exist... the sadness."))}}}]);
+//# sourceMappingURL=component---src-pages-404-js-6862bee547061620e0c4.js.map
